d3ba40ebc435fee3e41ec0d6322a0aa7c937d780
Ninh <nguyenhuuninhneu@gmail.com>
2021-12-21 00:35:45 +0700

    design for FAQ

--- src/components/settings/faq.js
+++ src/components/settings/faq.js
@@ -1,13 +1,84 @@
 import React, { Component } from 'react';
-import { Card, Link, Heading, Icon, Layout, Button, OptionList, Modal } from '@shopify/polaris'
+import { Card, Link, Heading, ResourceList, ResourceItem, TextStyle, Avatar, Button, ButtonGroup, TextField, Layout, SkeletonBodyText, SkeletonDisplayText, SkeletonPage, TextContainer, Icon } from '@shopify/polaris';
+import { CirclePlusMinor } from '@shopify/polaris-icons';
+import '../../assets/css/faq.css';
+import '../../App.css';
 
 class FAQ extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+
     render() {
         return (
-            <div>
-                FAQ
-            </div>
+            <div className={'faq'}>
+                <div className={'colLeft w30pc'}>
+                    <div className={'touch-section mb-20'}>
+                        <Card sectioned>
+                            <div className={'common-title title-fix'}>
+                                Getting in touch
+                            </div>
+                            <div className={'group-touch'}>
+                                <div className={'item-touch'}>
+                                    <div className={'item-left'}>
+                                        <Icon source={CirclePlusMinor} />
+                                    </div>
+                                    <div className={'item-right'}>
+                                        <div className={'btnEnableApp active'}>
+                                            <Button>Chat with us</Button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={'item-touch'}>
+                                    <div className={'item-left'}>
+                                        <Icon source={CirclePlusMinor} />
+                                    </div>
+                                    <div className={'item-right'}>
+                                        <Link url="https://help.shopify.com/manual">orichi247@gmail.com</Link>
+                                    </div>
+                                </div>
+                                <div className={'item-touch'}>
+                                    <div className={'item-left'}>
+                                        <Icon source={CirclePlusMinor} />
+                                    </div>
+                                    <div className={'item-right'}>
+                                        <Link url="https://help.shopify.com/manual">+84877566048</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                    <div className={'resources-section'}>
+                        <Card sectioned>
+                            <div className={'common-title'}>
+                                Resources
+                            </div>
+                            <div className={'group-resources'}>
+                                <div className={'item-resource'}>
+                                    <Heading>Inventory UI change</Heading>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+                <div className='colRight w66pc'>
+                    <div className={'Preview'}>
+                        <Card sectioned>
+                            <div className={'common-title'}>
+                                Preview
+                            </div>
+                        </Card>
+                    </div>
 
+                </div>
+                <div className={'cb'}>
+                </div>
+            </div>
         );
     }
 }
--- src/components/settings/setting.js
+++ src/components/settings/setting.js
@@ -1,9 +1,9 @@
-import React, { useState, Component } from 'react';
-import { Card, Button, TextField, Layout, SkeletonBodyText, SkeletonDisplayText, SkeletonPage, TextContainer } from '@shopify/polaris';
+import React, { Component } from 'react';
+import { Card, Button, ButtonGroup, TextField, Layout, SkeletonBodyText, SkeletonDisplayText, SkeletonPage, TextContainer } from '@shopify/polaris';
 import logo from '../../assets/images/tiktok.png'
 import logo1 from '../../assets/images/tiktok-1.png'
 import logo2 from '../../assets/images/tiktok-2.png'
-
+import '../../assets/css/setting.css';
 
 const listLogoData = [logo, logo1, logo2];
 
@@ -12,26 +12,52 @@ class Setting extends Component {
         super(props);
         this.state = {
             isEnable: true,
-            getLogo: logo
+            getLogo: logo,
+            indexActiveLogo: 0,
+            userName: ' ',
+            isBottom: true,
+            isLeft: true,
         }
         this.handleEnableApp = this.handleEnableApp.bind(this);
-
+        this.handleChangeUserName = this.handleChangeUserName.bind(this);
+        this.handleBottomTop = this.handleBottomTop.bind(this);
+        this.handleLeftRight = this.handleLeftRight.bind(this);
     }
 
     handleEnableApp() {
         this.setState(state => ({ isEnable: !state.isEnable }));
     }
-    changeLogo(logoSelected) {
+    changeLogo(logoSelected, index) {
         this.setState(state => ({ getLogo: logoSelected }));
+        this.setState(state => ({ indexActiveLogo: index }));
     }
+    handleChangeUserName(newValue) {
+        this.setState({ userName: newValue })
+    };
+
+    handleBottomTop() {
+        this.setState(state => ({ isBottom: !state.isBottom }));
+    }
+    handleLeftRight() {
+        this.setState(state => ({ isLeft: !state.isLeft }));
+    }
+
     render() {
+        const unitPos = '20px';
         const contentStatus = this.state.isEnable ? 'Enable App' : 'Disable App';
         const contentClassStatus = this.state.isEnable ? 'btnEnableApp active' : 'btnEnableApp';
-
+        const cssPositionLogo =
+            `.setting .colRight .Preview .logo img{`
+            + (this.state.isBottom ? `bottom:` + unitPos + `;` : `top:` + unitPos + `;`)
+            + (this.state.isLeft ? `left: ` + unitPos + `;` : `right:` + unitPos + `;`) +
+            +`}`;
         return (
             <div className={'setting'}>
                 <div className={'flexible-button flexible-p mb-20'}>
-                    <Card title="Flexible Tiktok Button" sectioned>
+                    <Card sectioned>
+                        <div className={'common-title'}>
+                            Flexible Tiktok Button
+                        </div>
                         <p>This plugin allows you to display a static/animated floating Tiktok logo or</p>
                         <p>This plugin allows you to display a static/animated fating Tiktok logo or Tikcode (on frontend) linked to your Tiktok account to increase your</p>
                         <p>Tiktok followers.</p>
@@ -43,17 +69,18 @@ class Setting extends Component {
                 </div>
                 <div className={'colLeft w50pc'}>
                     <div className={'button-style-section flexible-p mb-20'}>
-                        <Card title="Step 1: Choose the button style" sectioned>
+                        <Card sectioned>
+                            <div className={'common-title'}>
+                                Step 1: Choose the button style
+                            </div>
                             <p>Several options are offered as Tiktok button.</p>
                             <p>You may also use a custom image</p>
                             <div className={'group-logo'}>
-
                                 {
                                     listLogoData.map((logoitem, index) => {
-                                        debugger
                                         return (
-                                            <div key={index} className={'item-logo'}>
-                                                <Button onClick={() => { this.changeLogo(logoitem) }}>
+                                            <div key={index} className={this.state.indexActiveLogo == index ? 'item-logo active' : 'item-logo'}>
+                                                <Button onClick={() => { this.changeLogo(logoitem, index) }}>
                                                     <img src={logoitem} alt="" />
                                                 </Button>
                                             </div>
@@ -68,15 +95,44 @@ class Setting extends Component {
                         </Card>
                     </div>
                     <div className={'button-position-section flexible-p mb-20'}>
-                        <Card title="Step 2: Choose the button position" sectioned>
+                        <Card sectioned>
+                            <div className={'common-title'}>
+                                Step 2: Choose the button position
+                            </div>
+                            <div className={'group-position'}>
+                                <div className={'item-left'}>
+                                    <ButtonGroup segmented>
+                                        <div className={this.state.isBottom ? 'choose-bottom active' : ''}>
+                                            <Button onClick={this.handleBottomTop} >Bottom</Button>
+                                        </div>
+                                        <div className={!this.state.isBottom ? 'choose-top active' : ''}>
+                                            <Button onClick={this.handleBottomTop} >Top</Button>
+                                        </div>
+                                    </ButtonGroup>
+                                </div>
+                                <div className={'item-right'}>
+                                    <ButtonGroup segmented>
+                                        <div className={this.state.isLeft ? 'choose-left active' : ''}>
+                                            <Button onClick={this.handleLeftRight} >Left</Button>
+                                        </div>
+                                        <div className={!this.state.isLeft ? 'choose-right active' : ''}>
+                                            <Button onClick={this.handleLeftRight} >Right</Button>
+                                        </div>
+                                    </ButtonGroup>
+                                </div>
+                                <div className={'cb'}>
+                                </div>
+                            </div>
                             <div className={'title-username'}>
                                 Tiktok Username  <span className={'required'}>(*)</span>
                             </div>
-                            <div className={'w50pc'}>
+                            <div className={'w70pc mt-10'}>
                                 <TextField
-                                    placeholder="username"
+                                    placeholder="User name"
+                                    value={this.state.userName}
+                                    onChange={this.handleChangeUserName}
+                                    error={this.state.userName == '' ? 'User name is required' : ''}
                                     autoComplete="off"
-                                    className={'w50pc'}
                                 />
                             </div>
 
@@ -85,32 +141,35 @@ class Setting extends Component {
                 </div>
                 <div className='colRight w48pc'>
                     <div className={'Preview'}>
-                        <Card title="Preview" sectioned>
-                            <SkeletonPage primaryAction secondaryActions={2}>
-                                <Layout>
-                                    <Layout.Section>
-                                        <Card sectioned>
-                                            <SkeletonBodyText />
-                                        </Card>
-                                        <Card sectioned>
-                                            <TextContainer>
-                                                <SkeletonDisplayText size="small" />
+                        <Card sectioned>
+                            <div className={'common-title'}>
+                                Preview
+                            </div>
+                            <div className={'skeleton-section'}>
+                                <SkeletonPage primaryAction secondaryActions={2}>
+                                    <Layout>
+                                        <Layout.Section>
+                                            <Card sectioned>
                                                 <SkeletonBodyText />
-                                            </TextContainer>
-                                        </Card>
-                                        <Card sectioned>
-                                            <TextContainer>
-                                                <SkeletonDisplayText size="small" />
-                                                <SkeletonBodyText />
-                                            </TextContainer>
-                                        </Card>
-                                    </Layout.Section>
-                                </Layout>
-                            </SkeletonPage>
+                                            </Card>
+                                            <Card sectioned>
+                                                <TextContainer>
+                                                    <SkeletonDisplayText size="small" />
+                                                    <SkeletonBodyText />
+                                                </TextContainer>
+                                            </Card>
+                                        </Layout.Section>
+                                    </Layout>
+                                </SkeletonPage>
+                                <div className={'logo'}>
+                                    <style>
+                                        {cssPositionLogo}
+                                    </style>
+                                    <img src={this.state.getLogo} alt="" />
+                                </div>
+                            </div>
+
                         </Card>
-                        <div className={'logo'}>
-                            <img src={this.state.getLogo} alt="" />
-                        </div>
                     </div>
 
                 </div>
