dc51db329df720a0b414e5df167e2f7a7655b06e
Ninh <nguyenhuuninhneu@gmail.com>
2021-12-21 20:27:12 +0700

    responsive for mobile

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import en from '@shopify/polaris/locales/en.json';
 
 import App from "./App";
 import "@shopify/polaris/build/esm/styles.css";
+import"../src/assets/css/responsive.css";
+
 
 ReactDOM.render(<AppProvider i18n={en} theme={{colorScheme: "light"}}><App /></AppProvider>, document.getElementById("root"));
